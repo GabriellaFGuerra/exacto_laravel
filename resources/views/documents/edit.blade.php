2f45a43bc1a2bb887825e57f608128915587d3ad
Gabriella Guerra <gabriellafguera21@gmail.com>
2025-09-14 18:42:18 -0300

    Changed text color

--- resources/views/documents/edit.blade.php
+++ resources/views/documents/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <h2 class="font-semibold text-xl text-white leading-tight">
                 {{ __('Editar Documento') }} - {{ $document->name }}
             </h2>
             <a href="{{ route('documents.show', $document) }}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
